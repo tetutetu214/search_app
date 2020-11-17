b2bb482aae333c782f66b4ca900c73307ed7556c
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-17 11:29:17 +0900

    重複表示　削除実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all  # 全商品の情報を取得
+    set_product_column       # privateメソッド内で定義
   end
 
   def search
@@ -13,5 +14,9 @@ class ProductsController < ApplicationController
 
   def search_product
     @p = Product.ransack(params[:q])  # 検索オブジェクトを生成
+  end
+
+  def set_product_column
+    @product_name = Product.select("name").distinct  # 重複なくnameカラムのデータを取り出す
   end
 end
