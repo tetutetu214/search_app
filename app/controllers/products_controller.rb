3749d2edfcfacfdb07eb426425604bd3e609655e
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-17 11:12:11 +0900

    C　設定実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :search_product, only: [:index, :search]
+
   def index
+    @products = Product.all  # 全商品の情報を取得
   end
 
   def search
+    @results = @p.result.includes(:category)  # 検索条件にマッチした商品の情報を取得
+  end
+
+  private
+
+  def search_product
+    @p = Product.ransack(params[:q])  # 検索オブジェクトを生成
   end
 end
