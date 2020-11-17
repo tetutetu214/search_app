73135f57eb75e5d9c3b6ef5490c25a847415b1a8
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-17 11:52:22 +0900

    追加検索　実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,10 +4,12 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all  # 全商品の情報を取得
     set_product_column       # privateメソッド内で定義
+    set_category_column
   end
 
   def search
     @results = @p.result.includes(:category)  # 検索条件にマッチした商品の情報を取得
+    
   end
 
   private
@@ -18,5 +20,11 @@ class ProductsController < ApplicationController
 
   def set_product_column
     @product_name = Product.select("name").distinct  # 重複なくnameカラムのデータを取り出す
+    @product_size = Product.select("size").distinct
+    @product_status = Product.select("status").distinct
+  end
+  
+  def set_category_column
+    @category_name = Category.select("name").distinct
   end
 end
